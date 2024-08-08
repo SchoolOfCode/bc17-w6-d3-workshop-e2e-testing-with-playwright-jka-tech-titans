d92575d315bd172d50f7924bfbd0c3e40e8afa2d
Kiran Gadhavi <kirangadhavi@hotmail.com>
2024-08-08 12:24:10 +0100

    ticket 4 and add function goto and it passed

--- tests/todos.test.js
+++ tests/todos.test.js
@@ -3,3 +3,9 @@ import { test, expect } from "@playwright/test";
 test("practice test should pass", () => {
     
 })
+ test('test', async({page})=>{
+await page.goto('http://localhost:3000/');
+// await page.getByRole('input', { name: 'Add' }).click();
+// await page.getByRole('button', { name: 'Add' }).click();
+expect(page.url()).toBe('http://localhost:3000/');
+})
